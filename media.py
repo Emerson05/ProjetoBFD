da01b9e4241833995636495fdbd208eb90396e11
Emerson05 <emersonjuniormt@gmail.com>
2025-11-05 10:27:13 -0300

    inicio codigo de media

---
+++ media.py
@@ -0,0 +1,7 @@
+aluno = input("Digite o Nome do Aluno: ")
+nota1 = float(input("Digite a Primeira Nota: "))
+nota2 = float(input("Digite a Segunda Nota: "))
+
+media = (nota1+ nota2) / 2
+
+print(f"Aluno:{aluno}\n Média: {media}")
